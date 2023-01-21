5231d4c79a89071a789ade910a3a01ae98a1c581
unknown <dammajyoshna123@gmail.com>
2023-01-21 16:57:59 +0530

    code

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,8 +2,24 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    String name;
-    int Capacity;
+    private String name;
+    private int Capacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return Capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        Capacity = capacity;
+    }
 
     public Boat(String name, int capacity) {
         this.name = name;
