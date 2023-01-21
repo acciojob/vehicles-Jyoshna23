14178efd5b9c06c57fad365a0c1f26bcf8171046
unknown <dammajyoshna123@gmail.com>
2023-01-21 16:47:15 +0530

    changes made 2nd time

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,11 @@ public class Boat implements WaterVehicle{
     String name;
     int Capacity;
 
+    public Boat(String name, int capacity) {
+        this.name = name;
+        Capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,25 +4,26 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-
+        super(name,4,4,6,isManual,"F1",6);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
 
         newSpeed = getCurrentSpeed() + rate;
+
         if(newSpeed >= 1 && newSpeed <= 50){
-            setCurrentGear(1);
+            changeGear(1);
         }else if(newSpeed >= 51 && newSpeed <= 100){
-            setCurrentGear(2);
+            changeGear(2);
         }else if(newSpeed >= 101 && newSpeed <= 150) {
-            setCurrentGear(3);
+            changeGear(3);
         }else if(newSpeed >= 151 && newSpeed <= 200) {
-            setCurrentGear(4);
+            changeGear(4);
         }else if(newSpeed >= 201 && newSpeed <= 250) {
-            setCurrentGear(5);
+            changeGear(5);
         }else if(newSpeed >= 250) {
-            setCurrentGear(6);
+            changeGear(6);
         }
         /**
          * speed 0: gear 1
@@ -36,7 +37,8 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-            setCurrentGear(1);
+            super.stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
